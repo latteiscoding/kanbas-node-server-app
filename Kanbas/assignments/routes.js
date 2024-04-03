7cbb77c8e871c5b8e8abf31a5ac4241b4062e95a
latteiscoding <zjg.elaine@gmail.com>
2024-04-03 10:42:32 -0400

    node server 1st commit

---
+++ Kanbas/assignments/routes.js
@@ -0,0 +1,42 @@
+import db from "../Database/index.js";
+function AssignmentRoutes(app) {
+    // read
+    app.get("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const assignments = db.assignments
+            .filter((a) => a.course === cid);
+        res.send(assignments);
+    });
+
+    // create
+    app.post("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const newAssignment = {
+            ...req.body,
+            course: cid,
+            _id: new Date().getTime().toString(),
+        };
+        db.assignments.push(newAssignment);
+        res.send(newAssignment);
+    });
+
+    // update
+    app.put("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        const assignmentIndex = db.modules.findIndex(
+          (a) => a._id === aid);
+        db.assignments[assignmentIndex] = {
+          ...db.assignments[assignmentIndex],
+          ...req.body
+        };
+        res.sendStatus(204);
+      });
+
+    // delete
+    app.delete("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        db.assignments = db.assignments.filter((a) => a._id !== aid);
+        res.sendStatus(200);
+      });
+}
+export default AssignmentRoutes;
